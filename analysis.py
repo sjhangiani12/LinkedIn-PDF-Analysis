6c500292c02dd04444cbfce18fad1a1ebf6405f2
sjhangiani12 <sjhangiani12@users.noreply.github.com>
2017-12-22 17:37:44 -0800

    Finalized and submitted. All Systems Cleared and Passed. Signing off

--- analysis.py
+++ analysis.py
@@ -13,7 +13,6 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import glob, os
 import subprocess
-from cStringIO import StringIO
 
 globalExperienceCount = 0
 globalEducationCount = 0
@@ -211,5 +210,5 @@ for file in glob.glob("*.html"):
                                 df.set_value(newName, "Education Date " + str(indexEducationDate + 1), educationDate)
                         df.set_value(newName, "Education Type " + str(indexEducationDate + 1), educationType)
                 indexEducationDate += 1
-print df["Allison Cassels"]["Education Type "]
+print df
 print "DataFrame generated. SUCCESS."
